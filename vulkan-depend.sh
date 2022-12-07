92de96e944078b95d4c1a4308d8b10cf1a82b1f5
tomas-aragon <tomas-aragon@pm.me>
2022-12-07 10:43:22 -0300

    first commit

---
+++ vulkan-depend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install -y vulkan-tools vulkan-loader-devel vulkan-validation-layers-devel glfw-devel glm-devel glslc libXi-devel libXxf86vm-devel gcc-c++ cmake fontconfig-devel python git cmake
+
